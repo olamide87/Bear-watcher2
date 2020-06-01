8d0dc999799f7f2faf7169587c2edaf96cc3dcb1
Olamide Oladinni <olajide1987@gmail.com>
2020-05-31 23:39:23 -0500

    add bear form

---
+++ Javascripts/helpers/data/bearData.js
@@ -0,0 +1,15 @@
+
+
+const Bear = [
+
+]
+
+const getBears = () => {
+  return Bear 
+}
+
+const setSelectedBears = (newBear) => {
+  Bear.push(newBear);
+};
+
+export default { getBears, setSelectedBears }
---
+++ Javascripts/main.js
@@ -0,0 +1,11 @@
+import bearForm from "./Components/bearForm.js";
+import utils from "./helpers/utils.js";
+
+
+const init = () => {
+
+  bearForm.bearBuilder();
+    
+  };
+  
+  init();
